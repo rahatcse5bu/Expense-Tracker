ad0dfb2c79bf8ae8b39cf38189d3717d2109d55b
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-11-21 07:28:06 +0600

    some changes

--- Expense-Tracker/src/components/Transaction.jsx
+++ Expense-Tracker/src/components/Transaction.jsx
@@ -6,7 +6,7 @@ const color = transaction.amount>=0? 'green' : 'red';
 const sign = transaction.amount>=0? '+' : '-';
   return (
 // Using the Grid System with className
-<div className='transaction-item border-b'>
+<div className='transaction-item border-b mb-2 '>
 <div className={`grid grid-cols-2 gap-20 bg-blue-950 justify-between items-center text-white border-r-4 border-${color}-600 px-4 py-2`}>
   <div className="col-span-1"> {transaction.name}
   </div>
