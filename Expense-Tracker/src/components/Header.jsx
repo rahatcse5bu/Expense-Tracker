b98b6db0931c66140f1b8519de3af8fc047aa484
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-11-21 07:23:55 +0600

    Header design change

--- Expense-Tracker/src/components/Header.jsx
+++ Expense-Tracker/src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <div className='text-4xl text-blue-700 text-center mb-2 py-2 '>
+    <div className='text-4xl text-white text-center mb-2 pt-4 pb-8 rounded-br-[250px] rounded-bl-[250px] bg-blue-950'>
     Expense Tracker
  </div>
   )
--- Expense-Tracker/src/components/Transaction.jsx
+++ Expense-Tracker/src/components/Transaction.jsx
@@ -1,9 +1,7 @@
 // import React  from 'react';
 
-
 // eslint-disable-next-line react/prop-types
 export default function Transaction({transaction}) {
-//  console.log(transactions[0].name);
 const color = transaction.amount>=0? 'green' : 'red';
 const sign = transaction.amount>=0? '+' : '-';
   return (
