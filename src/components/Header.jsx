0fd1c4c823b5c421437bb45fe7dc2ee23e74489c
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-11-22 15:40:41 +0600

    mobile css fixed heading title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <div className='text-4xl text-white text-center mb-2 pt-4 pb-8 rounded-br-[250px] rounded-bl-[250px] bg-blue-950'>
+    <div className='text-3xl md:text-4xl text-white text-center mb-2 pt-4 pb-8 rounded-br-[250px] rounded-bl-[250px] bg-blue-950'>
     Expense Tracker
  </div>
   )
